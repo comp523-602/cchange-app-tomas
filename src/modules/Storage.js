6ea9f7b2b8789e03a01976779353e1a6d0c2d932
Tomas Roy <hello@minicreative.net>
2017-10-06 16:37:58 -0400

    cookie storage replaced with localStorage

--- src/modules/Storage.js
+++ src/modules/Storage.js
@@ -1,59 +1,73 @@
 
 var Storage = {
-	getAll: function () {
-
-		// Get cookies
-		var cookies = document.cookie;
-
-		// Catch empty cookies
-		if (cookies === "") return {};
-
-		// Initialize cookie library
-		var cookieLibrary = {};
-
-		// Make cookie array
-		var cookieArray = cookies.split(";");
-
-		// Push cookies into library
-		for (var i in cookieArray) {
-			var cookie = cookieArray[i];
-			var key, value;
-			for (var j in cookie) {
-				if (cookie[j] === "=") {
-					key = cookie.substring(0,j).replace(' ','');
-					value = cookie.substring((parseInt(j, 10)+1),cookie.length);
-					break;
-				}
-			}
-			cookieLibrary[key] = JSON.parse(value);
-		}
-
-		return cookieLibrary;
-
-	},
 	set: function (key, json) {
-
-		// Convert json to value string
 		var value = JSON.stringify(json);
-
-		// Setup default expiration (3 days with hour buffer)
-		var now = new Date().getTime();
-		var expiration = new Date(now + 1000 * 60 * 60 * 23 * 3).toUTCString();
-
-		// Add cookie
-		document.cookie = key + "=" + value + "; expires=" + expiration + ";";
+		window.localStorage.setItem(key, value);
 	},
 	get: function (key) {
-		var allCookies = this.getAll();
-		return allCookies[key];
-	},
-	delete: function (key) {
-		document.cookie = key + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/;';
+		var value = window.localStorage.getItem(key);
+		return JSON.parse(value);
 	},
 	clear: function () {
-		var allCookies = this.getAll();
-		for (var key in allCookies) this.delete(key);
+		window.localStorage.clear();
 	},
 };
+
+// var Storage = {
+// 	getAll: function () {
+//
+// 		// Get cookies
+// 		var cookies = document.cookie;
+//
+// 		// Catch empty cookies
+// 		if (cookies === "") return {};
+//
+// 		// Initialize cookie library
+// 		var cookieLibrary = {};
+//
+// 		// Make cookie array
+// 		var cookieArray = cookies.split(";");
+//
+// 		// Push cookies into library
+// 		for (var i in cookieArray) {
+// 			var cookie = cookieArray[i];
+// 			var key, value;
+// 			for (var j in cookie) {
+// 				if (cookie[j] === "=") {
+// 					key = cookie.substring(0,j).replace(' ','');
+// 					value = cookie.substring((parseInt(j, 10)+1),cookie.length);
+// 					break;
+// 				}
+// 			}
+// 			cookieLibrary[key] = JSON.parse(value);
+// 		}
+//
+// 		return cookieLibrary;
+//
+// 	},
+// 	set: function (key, json) {
+//
+// 		// Convert json to value string
+// 		var value = JSON.stringify(json);
+//
+// 		// Setup default expiration (3 days with hour buffer)
+// 		var now = new Date().getTime();
+// 		var expiration = new Date(now + 1000 * 60 * 60 * 23 * 3).toUTCString();
+//
+// 		// Add cookie
+// 		document.cookie = key + "=" + value + "; expires=" + expiration + ";";
+// 	},
+// 	get: function (key) {
+// 		var allCookies = this.getAll();
+// 		return allCookies[key];
+// 	},
+// 	delete: function (key) {
+// 		document.cookie = key + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/;';
+// 	},
+// 	clear: function () {
+// 		var allCookies = this.getAll();
+// 		for (var key in allCookies) this.delete(key);
+// 	},
+// };
 
 export default Storage;
