1f3ef70382e176991a585be974552b2cc30402f0
dflynt <flyntdw@unc.edu>
2017-12-12 21:31:00 -0500

    added css

--- src/components/User.js
+++ src/components/User.js
@@ -11,10 +11,15 @@ class User extends Component {
         return(
 			<Link to={'/user/' + this.props.user.guid}>
 				<div className="item update row">
+					{this.props.user.picture != null
+							? <img src={this.props.user.picture}/>
+							: null }
 					<div className="inside">
 						<div className="time">Joined {Format.time(this.props.user.dateCreated)}</div>
 						<h3>{this.props.user.name}</h3>
 						<p>{this.props.user.bio}</p>
+					</div>
+					<div className="clear">
 					</div>
 				</div>
   		  	</Link>
